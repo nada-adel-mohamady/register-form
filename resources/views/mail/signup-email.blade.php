8e10901d3ebd68352fd9d2c85bacd332d5d59fce
nada-adel-mohamady <39887982+nada-adel-mohamady@users.noreply.github.com>
2020-07-22 12:49:40 +0200

    new

---
+++ resources/views/mail/signup-email.blade.php
@@ -0,0 +1,12 @@
+Hello 
+<br><br>
+Welcome to my website 
+<br>
+Please click the below link to verfiy your email and activate your account !
+<br><br>
+<a href="http://localhost:8080/project2/public/verify?code={{$email_data['verification_code']}}">Click Here!</a>
+
+<br><br>
+Thank your!
+<br>
+coder 
